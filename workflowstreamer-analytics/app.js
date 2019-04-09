d18dced9af1440de00b0035a84f7b73c7acc0d9a
Salaheldin Akl <salaheldin.akl@student.dit.ie>
2019-04-09 02:47:35 +0100

    Connect to live DB (#11)

--- workflowstreamer-analytics/app.js
+++ workflowstreamer-analytics/app.js
@@ -3,23 +3,31 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const monk = require('monk');
 const eventsRouter = require('./routes/events');
 const usersRouter = require('./routes/users');
+const mongodb = require('mongodb');
 
-const db = monk('localhost:27017/test');
 const app = express();
+
+const MongoClient = mongodb.MongoClient;
+const uri = 'mongodb+srv://app:app123@analytics-cluster-v4gp0.gcp.mongodb.net/analytics?retryWrites=true';
+const mongoClient = new MongoClient(uri, { useNewUrlParser: true });
+
+mongoClient.connect((err) => {
+  if (err) {
+    console.log('ERR: Not Connected...')
+  } else {
+    const db = mongoClient.db('analytics')
+    console.log('DB Connected...')
+    app.set('db', db);
+  }
+});
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((req, res, next) => {
-  req.db = db;
-  next();
-});
 
 app.use('/events', eventsRouter);
 app.use('/users', usersRouter);
